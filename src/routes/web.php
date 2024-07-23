a03cc0d0997b1e55ea9c9e9904e81190ad342529
ishikawa shinya <manari526@gmail.com>
2024-07-23 22:40:58 +0900

    ルーティング更新

--- src/routes/web.php
+++ src/routes/web.php
@@ -24,6 +24,7 @@ Route::post('/register', [RegisteredUserController::class, 'store']);
 
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
 
 
 Route::get('/', [AtteController::class, 'index'])->middleware(['auth', 'verified']);
